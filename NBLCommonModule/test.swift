0b8f8dc1fe3c265d1afedafae5dcbf49c1d00090
koofrank <koofranker@gmail.com>
2018-09-10 15:44:53 +0800

    init

---
+++ NBLCommonModule/test.swift
@@ -0,0 +1,19 @@
+//
+//  test.swift
+//  NBLCommonModule
+//
+//  Created by koofrank on 2018/9/10.
+//  Copyright © 2018年 com.nbltrust. All rights reserved.
+//
+
+import Foundation
+
+class ttt {
+    func test() {
+        print("111")
+    }
+    
+    func test1() {
+        print("22")
+    }
+}
